9c231e18ebebe54cca989f5a584e6f507fe9d006
Fabian Andres Chiran <111092524+xFa00@users.noreply.github.com>
2022-11-28 01:29:23 -0500

    Update pregunta_12.sql

--- pregunta_12.sql
+++ pregunta_12.sql
@@ -42,6 +42,5 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-SELECT COUNT(*) FROM tbl1
-WHERE SUBSTR(c14, 1, 4) LIKE '2018'
-GROUP BY SUBSTR(c14, 1, 4);
+SELECT K0, MAX(c12), MIN(c12) FROM `tbl1` 
+GROUP BY K0
